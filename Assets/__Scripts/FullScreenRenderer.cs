f5cc8de4b12f55fecb3751dc89eb46af643240ef
Andy Agus <Redagus@gmail.com>
2022-06-12 12:04:41 -0400

    init commit 3

---
+++ Assets/__Scripts/FullScreenRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreenRenderer : MonoBehaviour {
+
+    private void Awake()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+        //find the height 
+        float cameraHeight = Camera.main.orthographicSize * 2;
+
+        Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+
+        Vector2 spriteSize = renderer.sprite.bounds.size;
+
+        Vector2 scale = transform.localScale;
+        //if height is greater than width
+        if(cameraSize.x >= cameraSize.y)
+        {
+            scale *= cameraSize.x / spriteSize.x;            
+        }
+        else
+        {
+            scale *= cameraSize.y / spriteSize.y;
+        }
+        transform.localScale = scale;
+    }
+}
---
+++ Assets/__Scripts/RotateTurret.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class RotateTurret : MonoBehaviour {
+    public GameObject turret;
+
+    
+    private void Update()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(CrossPlatformInputManager.mousePosition);
+        Vector3 direction = (mousePos - transform.position).normalized * 100;
+
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation.= Quaternion.Euler(angle, Vector3.forward);
+        
+        Debug.DrawLine(this.gameObject.transform.position, mousePos, Color.red);
+        
+    }
+}
