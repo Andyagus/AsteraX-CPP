186775d7bb6176618e70ece1d041b24734cfae3e
Andy Agus <Redagus@gmail.com>
2022-06-13 11:36:43 -0400

    added player movement and tilt with velocty

--- Assets/__Scripts/PlayerShip.cs
+++ Assets/__Scripts/PlayerShip.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class PlayerShip : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerShip : Singleton<PlayerShip>
+{
+    public float shipSpeed = 10f;
+    private Rigidbody shipRb;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        shipRb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        MoveShip();
+    }
+
+    private void MoveShip()
+    {
+        var horizontalInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        var verticalInput = CrossPlatformInputManager.GetAxis("Vertical");
+
+        var vel = new Vector3(horizontalInput, verticalInput);
+
+        if (vel.magnitude > 0)
+        {
+            vel.Normalize();
+        }
+
+        shipRb.velocity = vel * shipSpeed * Time.deltaTime;
+    }
+
 }
---
+++ Assets/__Scripts/TiltWithVelocity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltWithVelocity : MonoBehaviour {
+    public int degrees = 30;
+    public bool tiltTowards = true;
+
+    private int prevDegrees = int.MaxValue;
+    private float tan;
+
+    Rigidbody shipRb;
+
+    private void Start()
+    {
+        shipRb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        //dont completely understand
+        if (degrees != prevDegrees)
+        {
+            prevDegrees = degrees;
+            tan = Mathf.Tan(Mathf.Deg2Rad * degrees);
+        }
+
+        Vector3 pitchDir = tiltTowards ? -shipRb.velocity : shipRb.velocity;
+        pitchDir += Vector3.forward / tan * PlayerShip.instance.shipSpeed;
+        transform.LookAt(transform.position + pitchDir);
+    }
+}
